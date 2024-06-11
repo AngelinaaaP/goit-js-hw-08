9b84e6841cc0b5d640b82a2e82613f35eced7d90
Angelina Prodanyk <angelinaprodanyk@gmail.com>
2024-06-11 19:10:45 +0200

    add index file

--- js/task-3.js
+++ js/task-3.js
@@ -1,11 +1,10 @@
 function getElementWidth(content, padding, border) {
-  const contentNumber = 100(content.slice(0, -2));
-  const paddingNumber = 23(padding.slice(0, -2));
-  const borderNumber = 3(border.slice(0, -2));
-
-  const totalWidth = contentNumber + 2 * paddingNumber + 2 * borderNumber;
-
-  console.log(getElementWidth("50px", "8px", "4px")); // 74
-  console.log(getElementWidth("60px", "12px", "8.5px")); // 101
-  console.log(getElementWidth("200px", "0px", "0px")); // 200
+    const contentWidth = parseFloat(content);
+    const paddingWidth = parseFloat(padding) * 2;
+    const borderWidth = parseFloat(border) * 2;
+    return contentWidth + paddingWidth + borderWidth;
 }
+
+console.log(getElementWidth("50px", "8px", "4px")); // 74
+console.log(getElementWidth("60px", "12px", "8.5px")); // 101
+console.log(getElementWidth("200px", "0px", "0px")); // 200
